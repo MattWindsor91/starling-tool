d2453c42e315651f4468ea59feeb81323c1ecaad
Matt Windsor <m.windsor@imperial.ac.uk>
2019-07-01 16:49:23 +0100

    Try fixing travis build, round 1

--- install-z3.sh
+++ install-z3.sh
@@ -1,7 +1,7 @@
-if [[ ! -e z3-z3-4.4.1/install/lib/libz3.so ]]; then
-    wget https://github.com/Z3Prover/z3/archive/z3-4.4.1.zip
-    unzip z3-4.4.1.zip
-    cd z3-z3-4.4.1
+if [[ ! -e z3-z3-4.8.5/install/lib/libz3.so ]]; then
+    wget https://github.com/Z3Prover/z3/archive/z3-4.8.5.zip
+    unzip z3-4.8.5.zip
+    cd z3-z3-4.8.5
     python scripts/mk_make.py  --prefix=`pwd`/install
     cd build
     make
